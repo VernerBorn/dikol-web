6492b8f8308a6859ef904fa63ce931e55530aa83
VernerBorn <vernerborn@gmail.com>
2019-09-08 19:11:51 +0300

    add SVG icon to React Component

---
+++ src/Icons/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SvgButton = props => (
+  <svg viewBox="0 0 18 18" {...props}>
+    <path d="M7 16a2 2 0 104 0 2 2 0 10-4 0M0 9a2 2 0 104 0 2 2 0 10-4 0M14 9a2 2 0 104 0 2 2 0 10-4 0M7 2a2 2 0 104 0 2 2 0 10-4 0" />
+  </svg>
+)
+
+export default SvgButton
